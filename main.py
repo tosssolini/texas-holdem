f4539f4bb58b86d7a62dd63d76361db15614b920
Øyvind Torgersrud <oyvind.torgersrud@ngi.no>
2023-07-24 19:28:08 +0200

    draft game outline

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
 
 def main():
 
+    # User input number of players. Player class?
+
+    # Create deck
+
+
+    # Game loop:
+
+    # Deal hands (random card select and update deck)
+
+    # User input hold, raise, check
+
+    # Evaluate score (monte carlo?) 
 
 if __name__ == "__main__":
     main()
