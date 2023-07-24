b9d80f3632aa43b673b4904813e7f0624a280f5f
Øyvind Torgersrud <oyvind.torgersrud@ngi.no>
2023-07-24 20:02:27 +0200

    add player class and set up players

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 # Main
 
+import player
+
 def main():
 
-    # User input number of players. Player class?
+    # Set up players
+    players = []
+    number_of_players = int(input("User input number of players (Max 5): "))
+    my_name = input("Your name: ")
+    players.append(player.Player(my_name, True))
+
+    for i in range(number_of_players):
+        if i != 0:
+            name = input(f"Opponent {i} name: ")
+            players.append(player.Player(name, False))
+    
 
     # Create deck
-
 
     # Game loop:
 
     # Deal hands (random card select and update deck)
 
-    # User input hold, raise, check
+    # User input call, raise, fold
 
     # Evaluate score (monte carlo?) 
 
